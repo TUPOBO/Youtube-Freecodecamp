11c64cb63e2f69c65d3c6ac9551fa8a84b87e56b
TUPOBO <1028461931@qq.com>
2017-12-02 08:54:20 +0800

    RandomNumbers & parseInt()

---
+++ JavaScript Basics/17-RandomNumber-parselnt.js
@@ -0,0 +1,13 @@
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 20));
+
+function randomRange(min, max) {
+  return Math.floor(Math.random() * (min - max + 1)) + min;
+}
+
+console.log(randomRange(1, 9));
+
+
+parseInt('007'); // 7
+parseInt('t005'); // NaN
+parseInt('11', 2); // 3
